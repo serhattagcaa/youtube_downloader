a2c1387914e721918de9b46869b6a0823eb4e8ab
Serhat Ağca <serhathanza001@gmail.com>
2023-12-24 15:16:03 +0300

    other necessary libraries integrated v2

--- src/app.jsx
+++ src/app.jsx
@@ -8,26 +8,9 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
