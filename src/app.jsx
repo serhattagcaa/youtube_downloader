30868dee3ed9768acfab38053fa12592bea582f6
Serhat Ağca <serhathanza001@gmail.com>
2024-01-21 20:16:56 +0300

    I added <MovementGuide/> component

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import YoutubeMp3 from './components/youtubeMp3.jsx';
+import MovementGuide from './components/movementGuide.jsx';
 
 function App() {
   return (
     <body className='bg-[#f9f9f9] flex flex-col items-center justify-center '>
       <Navbar />
       <YoutubeMp3/>
+      <MovementGuide/>
     </body>
   );
 }
